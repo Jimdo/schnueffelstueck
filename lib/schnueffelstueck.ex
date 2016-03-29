fed00f120247e2b2b1e1fb4da9bb4e0c2926af24
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-03-29 09:47:46 +0200

    restructure config

--- lib/schnueffelstueck.ex
+++ lib/schnueffelstueck.ex
@@ -14,19 +14,22 @@ defmodule Schnueffelstueck do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    log_acceptor = :ranch.child_spec(:schnueffelstueck, @acceptors,
-      :ranch_tcp, [port: @port], Schnueffelstueck.Connection, [{:reporter, Schnueffelstueck.Reporter.Librato}, {:token, System.get_env("FASTLY_TOKEN")}])
-
     reporter_config = [
       {:user, System.get_env("LIBRATO_USER")},
       {:token, System.get_env("LIBRATO_TOKEN")},
       {:service, System.get_env("LIBRATO_PREFIX")}
     ]
 
+    parser_config = [
+      {:reporter, Schnueffelstueck.Reporter.Librato},
+      {:token, System.get_env("FASTLY_TOKEN")}
+    ]
+
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Domo.Worker, [arg1, arg2, arg3]),
-      log_acceptor,
+      :ranch.child_spec(:schnueffelstueck, @acceptors, :ranch_tcp,
+        [port: @port], Schnueffelstueck.Connection, parser_config),
       worker(Schnueffelstueck.Reporter.Librato, [reporter_config, [name: Schnueffelstueck.Reporter]])
     ]
 
