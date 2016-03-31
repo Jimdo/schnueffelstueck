2ff55b3454cabb97be918e139d24061bf3ae1699
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-03-31 15:38:12 +0200

    debug buffering

--- lib/schnueffelstueck/connection.ex
+++ lib/schnueffelstueck/connection.ex
@@ -47,19 +47,17 @@ defmodule Schnueffelstueck.Connection do
   def loop(socket, transport, buffer, token_reporter_mapping) do
     case transport.recv(socket, 0, @timeout) do
       {:ok, data} ->
-        case parse_line(String.split(buffer <> data, <<"\n">>, trim: true), []) do
-          {rest, []} -> loop(socket, transport, rest, token_reporter_mapping)
-          {rest, lines} ->
-            Enum.map(lines, fn(line) -> Task.start(__MODULE__, :parse_and_report, [line, token_reporter_mapping]) end)
-            loop(socket, transport, rest, token_reporter_mapping)
-        end
+        IO.puts inspect data
+        {rest, lines} = parse_lines(String.split(buffer <> data, <<"\n">>, trim: true), [])
+        Enum.map(lines, fn(line) -> Task.start(__MODULE__, :parse_and_report, [line, token_reporter_mapping]) end)
+        loop(socket, transport, rest, token_reporter_mapping)
       _ ->
         :ok = transport.close(socket)
     end
   end
 
-  def parse_line([rest | []], lines), do: {rest, Enum.reverse(lines)}
-  def parse_line([line | rest], lines), do: parse_line(rest, [line | lines])
+  def parse_lines([rest | []], lines), do: {rest, Enum.reverse(lines)}
+  def parse_lines([line | rest], lines), do: parse_lines(rest, [line | lines])
 
   def parse_and_report(line, mapping) do
     [_, line_token, data] = Regex.run(~r/^(.*)(\<.*)/, line)
